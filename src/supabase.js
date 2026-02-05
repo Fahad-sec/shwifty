9d9c8b5b7bd07a8c3015d5f25c45f0aeadeb413f
Fahad-sec <fahadakhtarabbasi@gmail.com>
2026-02-05 13:48:04 +0500

    v1.1.1 conenct to vercel

--- src/supabase.js
+++ src/supabase.js
@@ -2,3 +2,5 @@ const supaBaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supaBaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supaBase = window.supabase.createClient(supaBaseUrl, supaBaseKey)
+
+// push to vercel

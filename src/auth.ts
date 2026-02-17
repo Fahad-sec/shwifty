dca9eb47f612cd3cee434a65543f27c155c7e351
Fahad-sec <fahadakhtarabbasi@gmail.com>
2026-02-17 13:38:16 +0500

    3.6.1 add: convert auth file to TypeScript

---
+++ src/auth.ts
@@ -0,0 +1,181 @@
+import {supaBase} from './supabase.js'
+import {type AuthChangeEvent, type  Session } from '@supabase/supabase-js'
+
+const loader  = document.getElementById('loading-screen') as HTMLElement
+const loadingMsg = document.querySelector('.loading-msg') as HTMLElement
+    
+    const toggleLoader = (show: boolean, msg?: string) => {
+      if (!loader) return ;
+      if (show) {
+        loadingMsg.innerText  = msg || '';
+        loader.style.display = 'flex';
+        loader.classList.remove( 'loader-hidden')
+      } else {
+        loader.classList.add('loader-hidden')
+        setTimeout(() => loader.style.display = 'none', 500)
+      }
+    }
+
+
+   supaBase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null ) => {
+    console.log('Auth Event Types:', event)
+     const path = window.location.pathname
+    const isChatPage = path.includes('chatroom.html');
+    const isLoginPage = path.includes('index.html') || path.endsWith('/')
+
+    if (session) {
+
+      document.body.style.display = 'flex'
+      if (isLoginPage) {
+
+        console.log('user is logged in ', session.user)
+        window.location.href = "chatroom.html";
+      } 
+    } else {
+      document.body.style.display = 'flex'
+      console.log('no active session')
+      toggleLoader(false)
+
+      if (isChatPage) {
+        window.location.href = 'index.html';
+      } 
+    }
+}); 
+  
+let isLoginMode = true;
+
+const signupBtn = document.getElementById('signup-btn') as HTMLButtonElement;
+const loginBtn = document.getElementById('login-btn') as HTMLButtonElement;
+const logoutBtn = document.getElementById('logout-btn') as HTMLButtonElement;
+const authToggle = document.getElementById('auth-toggle') as HTMLButtonElement;
+const userName = document.getElementById('username') as HTMLInputElement;
+
+
+
+
+authToggle?.addEventListener('click', () => {
+  isLoginMode = !isLoginMode;
+
+  if (isLoginMode) {
+    authToggle.innerHTML = "Don't have an account?"
+    signupBtn.style.display = 'none'
+    userName.style.display = 'none'
+    loginBtn.style.display = 'flex'
+  } else {
+    loginBtn.style.display = 'none'
+    signupBtn.style.display = 'flex'
+    userName.style.display = 'flex'
+    authToggle.innerHTML = "Already have an account?"
+  }
+})
+
+
+logoutBtn?.addEventListener('click', async () =>{
+  const confirmed = confirm("Are you sure you want to logout?")
+  if (confirmed) {
+     toggleLoader(true, 'See you soon!')
+  logoutBtn.disabled = true;
+  logoutBtn.style.opacity = "0.7";
+  logoutBtn.style.cursor = "not-allowed";
+   
+   const {error} = await supaBase.auth.signOut();
+   toggleLoader(false)
+  if (error) {
+    toggleLoader(false)
+      logoutBtn.disabled = false;
+      logoutBtn.style.opacity = "1";
+      logoutBtn.style.cursor = "pointer";
+    console.error("logout error: ", error)
+  } else {
+    console.log('successfully logged out ')
+    window.location.href = "./index.html"
+  }
+
+  } else {
+    console.log('logout cancelled')
+  }
+
+  })
+
+
+loginBtn?.addEventListener('click', async () => {
+  toggleLoader(true, 'Verifying your account...' );
+  
+  const emailElement = document.getElementById('email') as HTMLInputElement;
+  const passwordElement = document.getElementById('password') as HTMLInputElement
+  if (emailElement && passwordElement) {
+    const email = emailElement.value;
+    const password = passwordElement.value;
+  
+
+
+      loginBtn.disabled = true;
+      loginBtn.innerText = "Authenticating..."
+      loginBtn.style.opacity = "0.7";
+      loginBtn.style.cursor = "not-allowed";
+
+    const {error } = await supaBase.auth.signInWithPassword({
+      email,
+      password
+  });
+
+      toggleLoader(false);
+      if (error) {
+        toggleLoader(false)
+      loginBtn.disabled = false;
+      loginBtn.innerText = "Login"
+      loginBtn.style.opacity = "1";
+      loginBtn.style.cursor = "pointer";
+        alert(error.message)
+      }else {
+        window.location.href = './chatroom.html'
+      }
+    }
+  })
+
+signupBtn?.addEventListener('click', async() => {
+  toggleLoader(true, 'Creating you account...' )
+    
+    const emailElement = document.getElementById('email') as HTMLInputElement;
+    const passwordElement = document.getElementById('password') as HTMLInputElement
+    const usernameElement = document.getElementById('username') as HTMLInputElement;
+
+    if (emailElement && passwordElement && usernameElement )
+    {
+      const email = emailElement.value;
+      const password = passwordElement.value;
+      const username = usernameElement.value
+    
+  
+
+      signupBtn.disabled = true;
+      signupBtn.innerText = "Signing up..."
+      signupBtn.style.opacity = "0.7";
+      signupBtn.style.cursor = "not-allowed";
+  
+
+    const {error } = await supaBase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {username: username, display_name: username}
+      }
+  });
+
+    toggleLoader(false)
+    if (error) {
+      toggleLoader(false)
+        signupBtn.disabled = false;
+        signupBtn.innerText = "Sign up"
+        signupBtn.style.opacity = "1";
+        signupBtn.style.cursor = "pointer";
+      alert(error.message);
+    } else {
+      alert('check your email for a confirmation linK!')
+    }
+
+  }
+  })
+
+
+
